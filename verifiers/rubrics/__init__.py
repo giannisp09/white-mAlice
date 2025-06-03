8e288214c025a856c6a282e44ac2ac0beea642ed
SOCAIT <socait@Gianniss-MacBook-Pro.local>
2025-06-03 22:25:39 +0300

    added pentest rubric

--- verifiers/rubrics/__init__.py
+++ verifiers/rubrics/__init__.py
@@ -5,6 +5,7 @@ from .math_rubric import MathRubric
 from .codemath_rubric import CodeMathRubric
 from .tool_rubric import ToolRubric
 from .smola_tool_rubric import SmolaToolRubric
+from .codepentest_rubric import CodePentestRubric
 
 __all__ = [
     "Rubric",
@@ -13,5 +14,6 @@ __all__ = [
     "MathRubric",
     "CodeMathRubric",
     "ToolRubric",
-    "SmolaToolRubric"
+    "SmolaToolRubric",
+    "CodePentestRubric"
 ]
